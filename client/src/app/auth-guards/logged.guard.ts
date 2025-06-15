c746136fc56416e2feb331ccdabc19ea6d70c143
Daniele Moscati <danielemoscati96@hotmail.it>
2025-06-15 19:37:02 +0200

    Aggiornamento Angular v16

--- client/src/app/auth-guards/logged.guard.ts
+++ client/src/app/auth-guards/logged.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
@@ -7,7 +7,7 @@ import { AuthService } from '../services/auth.service';
   providedIn: 'root'
 })
 
-export class LoggedGuard implements CanActivate {
+export class LoggedGuard  {
 
   constructor(private router: Router, private authService: AuthService) { }
 
--- client/src/app/auth-guards/nologged.guard.ts
+++ client/src/app/auth-guards/nologged.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NologgedGuard implements CanActivate {
+export class NologgedGuard  {
 
   constructor(private router: Router, private authService: AuthService) { }
   
