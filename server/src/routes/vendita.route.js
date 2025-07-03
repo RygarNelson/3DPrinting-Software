10ddd6d0bfa54b2a845f220fb45913687a48319c
Daniele Moscati <danielemoscati96@hotmail.it>
2025-07-03 14:15:43 +0200

    Server: in scadenza e scaduto

--- server/src/routes/vendita.route.js
+++ server/src/routes/vendita.route.js
@@ -257,6 +257,22 @@ router.post(
                 }
             }
 
+            if (req.body.isInScadenza || req.body.isScaduto) {
+                let dataOggi = new Date();
+                dataOggi.setTime( dataOggi.getTime() - dataOggi.getTimezoneOffset()*60*1000 );
+
+                const valoriAmmessiStatoSpedizione = [0, 4];
+
+                if (req.body.isInScadenza) {
+                    whereOptions.data_scadenza = { [Op.gte]: dataOggi };
+                    whereOptions.data_scadenza_spedizione = { [Op.lt]: dataOggi };
+                } else {
+                    whereOptions.data_scadenza_spedizione = { [Op.gte]: dataOggi };
+                }
+
+                whereOptions.stato_spedizione = { [Op.in]: valoriAmmessiStatoSpedizione };
+            }
+
             const limit = req.body.limit ? parseInt(req.body.limit) : 10;
             const offset = req.body.offset ? parseInt(req.body.offset) : 0;
 
@@ -268,6 +284,19 @@ router.post(
                 'data_scadenza_spedizione',
                 'totale_vendita',
                 'stato_spedizione',
+                [literal(`
+                    CASE
+                        WHEN stato_spedizione IN (0, 4)
+                            AND data_scadenza >= CURRENT_DATE
+                            AND data_scadenza_spedizione < CURRENT_DATE
+                        THEN 1 ELSE 0 END
+                `), 'isInScadenza'],
+                [literal(`
+                    CASE
+                        WHEN stato_spedizione IN (0, 4)
+                            AND data_scadenza_spedizione >= CURRENT_DATE
+                        THEN 1 ELSE 0 END
+                `), 'isScaduto'],
                 [literal(`
                     CASE
                         WHEN stato_spedizione = 4 THEN 1
